0b8dd519d9c6165cda933fe0e92f3a1f517f18fa
cookieroz <cookieroz@gmail.com>
2015-09-29 09:12:42 -0700

    Weekly backup - September 29, 2015

---
+++ wp-content/themes/rachel-makeup/homeRGunderConstruction.php
@@ -0,0 +1,26 @@
+<?php /* Template Name: RGunderConstruction */ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Nixie+One|Comfortaa|Fredoka+One|Leckerli+One|Stint+Ultra+Condensed' rel='stylesheet' type='text/css'>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+  <div class="under-construction">
+    <h1>Currently Under Construction</h1>
+    <p>Feel free to browse my Facebook or Instagram account</p>
+    <div class="social-media-squares">
+      <a href="http://facebook.com/raquelgarciamua" target="_blank"><i class="fa fa-facebook"></i></a>
+      <a href="http://instagram.com/rgartistry" target="_blank"><i class="fa fa-instagram"></i></a>
+    </div>
+  </div><!-- #primary -->
+<?php wp_footer(); ?>
+</body>
+</html>
